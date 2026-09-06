533240db5ce2c7fdedeef1106e11c5d68410928d
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-09-06 14:47:28 +0530

    update the age group categry sol

--- 02_conditionals/age_group_categ.py
+++ 02_conditionals/age_group_categ.py
@@ -2,9 +2,9 @@ age = int(input("Enter your age: "))
 
 if age < 13 and age >= 0:
     print("You are a child.")   
-elif age < 19 and age >= 13:
+elif age < 19:
     print("You are a teenager.")            
-elif age <= 59 and age >= 20:
+elif age <= 59:
     print("You are an adult.")
 elif age >= 60:
     print("You are a senior citizen.")  
