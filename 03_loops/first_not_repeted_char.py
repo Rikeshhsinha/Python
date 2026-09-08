ffe4d6dafaa713a3bf5b9115087373072428dcbe
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-09-08 22:36:41 +0530

    Program to find the first non-repeated character in a string

---
+++ 03_loops/first_not_repeted_char.py
@@ -0,0 +1,8 @@
+string = input("Enter a string: ")
+
+for char in string:
+    if string.count(char) == 1:
+        print("First non-repeated character:", char)
+        break
+else:
+    print("No non-repeated character found.")
